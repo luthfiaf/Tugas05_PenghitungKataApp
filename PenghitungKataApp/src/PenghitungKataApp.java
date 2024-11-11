747f0f8ef008fac78661f99317fbf8d7562e381b
muhammad.arfn26@gmail.com <muhammad.arfn26@gmail.com>
2024-11-11 20:57:12 +0800

    membuat aplikasi penghitung kata

---
+++ PenghitungKataApp/src/PenghitungKataApp.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author ASUS
+ */
+public class PenghitungKataApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PenghitungKataView view = new PenghitungKataView();
+        view.setVisible(true);
+    }
+
+    private static class PenghitungKataView {
+
+        public PenghitungKataView() {
+        }
+
+        private void setVisible(boolean b) {
+            throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        }
+    }
+    }
+    
+
